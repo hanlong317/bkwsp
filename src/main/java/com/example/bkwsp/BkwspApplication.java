1fa4f7828a6d7695164ed1ea83b999c9d12c89fe
han_long <1306507227@qq.com>
2020-10-25 21:16:26 +0800

    首次提交

---
+++ src/main/java/com/example/bkwsp/BkwspApplication.java
@@ -0,0 +1,14 @@
+package com.example.bkwsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class BkwspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkwspApplication.class, args);
+    }
+
+}
